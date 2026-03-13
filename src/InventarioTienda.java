2c68161d1839b5e56f298279e51bc88b72b9f2e1
n3tz-3 <arot324@gmail.com>
2026-03-13 19:16:33 +0100

    Error 4

--- src/InventarioTienda.java
+++ src/InventarioTienda.java
@@ -54,7 +54,10 @@ public class InventarioTienda {
     public void listarProductos() {
         System.out.println("Listado de productos:");
         for (int i = 0; i < contador; i++) {
-            System.out.println("Posición " + i + ": " + productos[i]);
+            if (productos[i] != null) {
+                System.out.println("Posición " + i + ": " + productos[i]);
+            }
+
         }
     }
 
